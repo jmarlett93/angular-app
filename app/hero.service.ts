d0148535e192f0f091e116eb0e611e38669be32b
John Marlett <1fiyero@gmail.com>
2016-08-05 15:59:28 -0500

    builing out routing functions

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,4 +8,17 @@ export class HeroService {
   getHeroes() {
     return Promise.resolve(HEROES);
   }
+  
+  getHeroesSlowly() {
+  return new Promise<Hero[]>(resolve =>
+    setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+  );
+  
+  }
+  
+  getHero(id: number) {
+      return this.getHeroes()
+                 .then(heroes => heroes.find(hero => hero.id === id));
+  }
+
 }
